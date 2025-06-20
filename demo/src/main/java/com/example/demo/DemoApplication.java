965b56625490d7592349d41926ba95c464a6d9fd
Badsena <sena@amypo.in>
2025-06-20 19:58:24 +0000

    User

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		test 2
+		test 3
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
